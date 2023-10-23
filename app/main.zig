4f88ca859cde3011a3afabf5d062ceefd2c0fdf6
Gerry Agbobada <gerry@gagbo.net>
2023-10-23 22:05:18 +0200

    Read the number of tables in the database

--- app/main.zig
+++ app/main.zig
@@ -27,10 +27,39 @@ pub fn main() !void {
         var file = try std.fs.cwd().openFile(database_file_path, .{});
         defer file.close();
 
+        const dbInfo = try DBInfo.read(file);
+
+        try std.io.getStdOut().writer().print("database page size: {}\n", .{dbInfo.page_size});
+        try std.io.getStdOut().writer().print("number of tables: {}\n", .{dbInfo.table_count});
+    }
+}
+
+const DBInfo = struct {
+    page_size: u16,
+    table_count: u16,
+
+    pub fn read(file: std.fs.File) !DBInfo {
         var buf: [2]u8 = undefined;
         _ = try file.seekTo(16);
         _ = try file.read(&buf);
         const page_size = std.mem.readInt(u16, &buf, .Big);
-        try std.io.getStdOut().writer().print("database page size: {}\n", .{page_size});
+
+        // The header is 100 bytes long https://www.sqlite.org/fileformat.html#the_database_header
+        // After the header, we have the actual beginning of the first page that is the root page of the table b-tree for
+        // the sqlite_schema table https://www.sqlite.org/fileformat.html#storage_of_the_sql_database_schema
+
+        var page_type: [1]u8 = undefined;
+        _ = try file.seekTo(100);
+        _ = try file.read(&page_type);
+
+        // We assert that the page is a table _leaf_ page, meaning we can only count the cells to get the table count
+        std.debug.assert(page_type[0] == 0x0D);
+
+        var buffer: [2]u8 = undefined;
+        _ = try file.seekBy(2);
+        _ = try file.read(&buffer);
+        const table_count = std.mem.readInt(u16, &buffer, .Big);
+
+        return DBInfo{ .page_size = page_size, .table_count = table_count };
     }
-}
+};
